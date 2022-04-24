cdb310120ce91b644d9dda19832b3a7169160c9d
Jaroslaw Charchula <charchula@gmail.com>
2022-04-24 19:19:35 +0200

    Update app.js file with countDown fun, forEach with eventListener

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 
 const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
-const timeLeft = document.querySelector("#timie-left");
+const timeLeft = document.querySelector(".time-left");
 const score = document.querySelector('#score');
 
 let result = 0;
+let hitPosition
+let currentTime = 60;
+let timerId = null;
 
 function randomSquare() {
     squares.forEach(square => {
@@ -15,7 +18,34 @@ function randomSquare() {
     let randomSquare = squares[Math.floor(Math.random() * 9)]
     randomSquare.classList.add('mole');
 
+    hitPosition = randomSquare.id
 }
 
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id === hitPosition) {
+            result++
+            score.textContent = result;
+            hitPosition = null;
+        }
+    })
+})
 
-randomSquare()
+function moveMole() {    
+    timerId = setInterval(randomSquare, 1000);
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+   
+    if (currentTime == 0) {
+      clearInterval(countDownTimerId)
+      clearInterval(timerId)
+      alert('GAME OVER! Your final score is ' + result)
+    }
+}
+
+let countDownTimerId =  setInterval(countDown, 1000);
